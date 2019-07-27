45c9f0697bb4058d1165b59b73b68331ed0f5afa
Thiago <thiago.empreendedor01@gmail.com>
2019-07-27 10:46:52 -0300

    adicionando componente de  display

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,12 +1,15 @@
 import React, {Component} from 'react'
 import './Calculator.css'
 import Button from '../components/Button';
+import Display from '../components/Display';
+
 
 export default class Calculator extends Component {
 
     render() {
         return (
             <div className="calculator">
+                <Display value={100} />
                 <Button label="AC"></Button>
                 <Button label="/"></Button>
                 <Button label="7"></Button>
